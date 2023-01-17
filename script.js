38f97030f6361d1749aabf0edfbd5e9ce6ad64fe
aleksmarsha <27923826+aleksmarsha@users.noreply.github.com>
2023-01-17 02:05:38 +0200

    Create the function for play round

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 function getComputerChoice() {
     let artificialChoice = Math.floor(Math.random() * 3);
     if (artificialChoice===0) {
-        return "Rock";
+        return `Rock`;
     } else if (artificialChoice===1) {
-        return "Paper";
+        return `Paper`;
     } else {
-        return "Scissors";
+        return `Scissors`;
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    if (playerSelection === computerSelection) {
+        return `It's a tie!`;
+    } else if ((playerSelection===`Rock` && computerSelection===`Scissors`) || (playerSelection===`Paper` && computerSelection===`Rock`) || (playerSelection===`Scissors` && computerSelection===`Paper`)) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
